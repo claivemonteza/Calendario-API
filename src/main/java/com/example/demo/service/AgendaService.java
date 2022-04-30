afdfa833dd4eb9c324c8dd43d2e0e0b6a00bb69a
Claive Monteza <43734209+claivemonteza@users.noreply.github.com>
2022-04-30 02:04:38 +0200

    Add method findByDate

--- src/main/java/com/example/demo/service/AgendaService.java
+++ src/main/java/com/example/demo/service/AgendaService.java
@@ -1,20 +1,26 @@
 package com.example.demo.service;
 
+import java.util.Date;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Service;
 
 import com.example.demo.domain.Agenda;
-import com.example.demo.filebd.File;
+import com.example.demo.filebd.Ficheiro;
 
 @Service
 public class AgendaService {
 
 	public void save(Agenda agenda) throws Exception {
-		File.WriteOnTheFile(agenda);
+		Ficheiro.WriteOnTheFile(agenda);
 	}
 
 	public List<Agenda> list() throws Exception{
-		return File.readTheFile();
+		return Ficheiro.readTheFile();
+	}
+	
+	public List<Agenda> findByDate(Date date) throws Exception{
+		return Ficheiro.readTheFile().stream().filter(a->a.getData().equals(date)).collect(Collectors.toList());
 	}
 }
