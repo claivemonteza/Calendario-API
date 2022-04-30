3f28d5c195c74f38a39a83c8c1cdde8fe19c0399
Claive Monteza <43734209+claivemonteza@users.noreply.github.com>
2022-04-30 02:05:55 +0200

    Add validation and json format

--- src/main/java/com/example/demo/dto/AgendaDTO.java
+++ src/main/java/com/example/demo/dto/AgendaDTO.java
@@ -1,17 +1,23 @@
 package com.example.demo.dto;
 
+import javax.validation.constraints.NotBlank;
+
 import com.example.demo.domain.Agenda;
 import com.example.demo.util.DateUtil;
+import com.fasterxml.jackson.annotation.JsonFormat;
 
 import lombok.Data;
 
 @Data
 public class AgendaDTO {
 
+	@NotBlank(message = "Please enter a Date (yyyy-MM-dd)")
+	@JsonFormat(pattern = "yyyy-MM-dd")
 	private String data;
+	@NotBlank(message = "Please enter a Information")
 	private String informacao;
 	
 	public Agenda toEntity() throws Exception {
-		return new Agenda(DateUtil.parseString(data), informacao);
+		return new Agenda(DateUtil.formatStringToDate(data), informacao);
 	}
 }
