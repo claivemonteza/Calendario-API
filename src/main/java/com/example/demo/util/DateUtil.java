2b14335aaf3a9328700ca16cda5e93a4a7d3262f
Claive Monteza <43734209+claivemonteza@users.noreply.github.com>
2022-04-30 02:03:27 +0200

    Modify the method on class DateUtil

--- src/main/java/com/example/demo/util/DateUtil.java
+++ src/main/java/com/example/demo/util/DateUtil.java
@@ -1,17 +1,23 @@
 package com.example.demo.util;
 
-
 import java.text.DateFormat;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-
 public class DateUtil {
 
-	public static Date parseString(String str) throws Exception {
-		DateFormat formatter = new SimpleDateFormat("E MMM dd HH:mm:ss Z yyyy");
-		Date date = (Date)formatter.parse(str);
+
+	public static Date formatStringToDate(String str) throws Exception {
+		Date date = null;
+		DateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
+		try {
+			date = (Date) formatter.parse(str);
+		}catch (ParseException e) {
+			formatter = new SimpleDateFormat("E MMM dd HH:mm:ss Z yyyy");
+			date = (Date) formatter.parse(str);
+		}
 		return date;
 	}
-		
+
 }
