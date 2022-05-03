4986408e87371fa1949023b85698943e198566c7
Claive Monteza <43734209+claivemonteza@users.noreply.github.com>
2022-05-03 02:04:07 +0200

    Add Constants

---
+++ src/main/java/com/example/demo/constants/ApiConstants.java
@@ -0,0 +1,9 @@
+package com.example.demo.constants;
+
+public class ApiConstants {
+	
+	public static final String FILE_NAME = "Agendamentos";
+	
+	public final static String TEMP_FILE = "Temp";
+	
+}
