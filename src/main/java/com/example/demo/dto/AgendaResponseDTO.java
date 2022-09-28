b57581f88c94533097b2b53f705bd103319a8379
Claive Monteza <43734209+claivemonteza@users.noreply.github.com>
2022-09-28 22:31:54 +0200

    Add json format

--- src/main/java/com/example/demo/dto/AgendaResponseDTO.java
+++ src/main/java/com/example/demo/dto/AgendaResponseDTO.java
@@ -2,13 +2,14 @@ package com.example.demo.dto;
 
 import com.example.demo.domain.Agenda;
 import com.example.demo.util.DateUtil;
+import com.fasterxml.jackson.annotation.JsonFormat;
 
 import lombok.Data;
 
 @Data
 public class AgendaResponseDTO {
 
-	
+	@JsonFormat(pattern = "EEE MMM dd yyyy")
 	private String data;
 	private String informacao;
 	
